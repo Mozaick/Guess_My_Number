ce475442e27bfddc454f49daed6ede7dc3132141
Mozaick <mozeselbarbary@developersinstitute.co.nz>
2022-01-19 11:35:37 +1300

    more case cenarios

--- scripts.js
+++ scripts.js
@@ -8,8 +8,8 @@ const btnCheck = document.querySelector('.check');
 
 // DOM Elms - </p>
 ////////////////////
-// '?' -> '.number'
-const pElmNum = document.querySelector('.number');
+// '?' -> '.num'
+const pElmNum = document.querySelector('.num');
 // 'start guessing...' -> '.message'
 const pElmMsg = document.querySelector('.msg');
 
@@ -23,3 +23,43 @@ const spanElmHighScore = document.querySelector('.high-score');
 // DOM Elms - </input>
 // '' -> '.guess'
 const inputElm = document.querySelector('.guess');
+
+// WHEN '.check' btn clicked
+// IF '.guess' is empty || 0 || NAN
+// '.msg' changes to "must be number greater than 0"
+// && '.num' changes to Emoji
+btnCheck.addEventListener('click', () => {
+//   Number(inputElm.value);
+//     console.log(inputElm.value, typeof Number(inputElm.value), isNaN(inputElm));
+const inputElm = Number(document.querySelector('.guess'));
+console.log(inputElm, inputElm.value, typeof inputElm);
+  if (inputElm === '' || inputElm === 0 || isNaN(inputElm)) {
+    pElmMsg.textContent = '⛔️ must be number greater than 0';
+    pElmNum.textContent = '🎭';
+    function clearNdAutofocus() {
+      inputElm.value = null;
+      inputElm.focus();
+    }
+    setTimeout(clearNdAutofocus, 1000);
+  }
+});
+
+// WHEN you input a num and click '.check' btn
+// a random num generates, and shows up in '.num'
+
+// then
+// IF random num ≠≠ inputted num
+// '.score' goes down one
+
+// IF random num == inputted num
+// value of that inputted num adds to '.high-score'
+
+// then you should click '.again' btn to reset
+
+// WHEN '.again' btn clicked, '.guess' clears out
+// && '.score' resets to 20
+
+// '.high-score' changes from current value to something else
+// IF inputted num == random num
+// && inputted num > current '.high-score' value
+// ELSE WHEN '.again' btn clicked '.high-score' == current
